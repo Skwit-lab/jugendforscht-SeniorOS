916707dc14520d3ccdffdd2be69bd77a619a6839
Skwit01 <180455812+Skwit-lab@users.noreply.github.com>
2026-07-05 11:40:01 +0200

    Aufräumen

--- t1/Installv1t1/src/libs/install/mod.rs
+++ t1/Installv1t1/src/libs/install/mod.rs
@@ -69,29 +69,3 @@ pub struct InstallConfig {
     pub partitions: Vec<PartitionConfig>,
 }
 
-impl PartitionConfig {
-    pub fn default_layout() -> Vec<PartitionConfig> {
-        vec![
-            PartitionConfig {
-                mountpoint: "/boot".into(),
-                size: "1G".into(),
-                filesystem: "ext2".into(),
-            },
-            PartitionConfig {
-                mountpoint: "/".into(),
-                size: "0".into(),
-                filesystem: "ext4".into(),
-            },
-            PartitionConfig {
-                mountpoint: "/home".into(),
-                size: "0".into(),
-                filesystem: "ext4".into(),
-            },
-            PartitionConfig {
-                mountpoint: "swap".into(),
-                size: "8G".into(),
-                filesystem: "linux-swap".into(),
-            },
-        ]
-    }
-}
